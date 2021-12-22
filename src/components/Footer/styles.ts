718df75c5afe0ea123fff13ec359c37f4d74654d
MicaioValente <micaiovalente@gmail.com>
2021-12-22 15:44:33 -0300

    footer

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -5,11 +5,14 @@ export const Footer = styled.footer`
     right: 0px;
     left: 0px;
     padding: 0.5rem 0 0 0;
-    font-family: roboto, sans-serif;
-    background-color: #F2F2F2;
+    font-family: roboto,sans-serif;
     text-align: center;
     margin: 0;
     font-weight: 600;
+    height: 2rem;
+    p{
+        margin: 0;
+    }
 
 `
 export const DivImageContainer = styled.div`
