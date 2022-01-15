265c411506710098ac41e5ade032c04dc30d1c2e
Alesandro Fidelis <alesandro.fidelis@softplan.com.br>
2022-01-15 11:26:47 -0300

    feat

--- src/components/FormCreatedSimulated/index.tsx
+++ src/components/FormCreatedSimulated/index.tsx
@@ -57,6 +57,7 @@ export default function FormCreatedSimulated() {
     const [youtubeOrThumbnailSelected, setYoutubeOrThumbnailSelected] = useState("")
     const [OrderQuestionsSelected, setOrderQuestionsSelected] = useState<number>(0)
     const [time, setTime] = useState<Time>({ tempoPorProva: '' })
+    const [formDataThumbnail, setformDataThumbnail] = useState<any>(null)
     const router = useRouter()
     // const FakeUser = {
     //     author: 'Usuario'
@@ -79,7 +80,7 @@ export default function FormCreatedSimulated() {
         const newObject = Object.assign(values, time)
         console.log("newObject", newObject)
         // setFormSimuled(newObject)
-        // postSimuled(newObject)
+        postSimulated(newObject)
     };
 
     function orderQuestions(e) {
@@ -88,24 +89,37 @@ export default function FormCreatedSimulated() {
     }
 
     async function postSimulated(newObject) {
+        await api.post('api/Simulado', newObject)
+            .then(response => {
+                console.log("response simu", response)
+                if (response) {
+                    postThumbnail(response.data.id)
+                }
 
-        await api.post('api/Simulado', newObject, {
-
-        }).then(function () {
-            router.push('/')
-            toast.success('Simulado salvo com sucesso ')
-        }).catch(function (error) {
-            toast.error(`Um erro inesperado aconteceu ${error.response.status}`)
-            setIsSpinning(false)
-        });
+            }).catch(function (error) {
+                toast.error(`Um erro inesperado aconteceu ${error.response.status}`)
+                setIsSpinning(false)
+            });
     }
 
-    const normFile = (e) => {
-        console.log('Upload event:', e);
-        if (Array.isArray(e)) {
-            return e;
-        }
-        return e && e.fileList;
+    async function postThumbnail(id) {
+        console.log("id", id)
+        console.log("thumbnail", formDataThumbnail)
+        const archive = new FormData()
+        archive.append('arquivo', formDataThumbnail)
+
+        await api.post(`api/Simulado/upload-thumbnail/${id}`, archive)
+            .then(function () {
+                router.push('/')
+                toast.success('Simulado salvo com sucesso ')
+            }).catch(function (error) {
+                toast.error(`Um erro inesperado aconteceu ${error.response.status}`)
+                setIsSpinning(false)
+            });
+    }
+
+    const normFile = (file: any, fileList: any) => {
+        setformDataThumbnail(file)
     };
 
     function youtubeOrThumbnail(e) {
@@ -158,12 +172,20 @@ export default function FormCreatedSimulated() {
                 {youtubeOrThumbnailSelected == "thumbnail" ?
                     (
                         <Form.Item
-                            name="thumbnail"
-                            label="Upload"
-                            valuePropName="fileList"
-                            getValueFromEvent={normFile}
+                            name='clientImage'
+                            label="Capa do simulado"
+                            rules={[{ required: true, message: 'Por favor insira uma imagem' }]}
+                            style={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "flex-end"
+                            }}
                         >
-                            <Upload name="logo" action="/upload.do" listType="picture">
+                            <Upload
+                                listType="picture"
+                                beforeUpload={normFile}
+                                accept=".png"
+                            >
                                 <Button style={{ color: '#000000D9', border: '1px solid #d9d9d9' }} icon={<UploadOutlined />}>Click to upload</Button>
                             </Upload>
                         </Form.Item>
@@ -172,7 +194,6 @@ export default function FormCreatedSimulated() {
                             <Form.Item
                                 name='linkYoutube'
                                 label="Youtube Link"
-
                             >
                                 <Input addonBefore="youtube.com/" defaultValue="mysite" />
                             </Form.Item>
